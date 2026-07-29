d94a2eb37dc9b553baaad8996a6f0d446c1876dc
Yashi Saini <yashisaini718@gmail.com>
2026-07-29 21:20:05 +0530

    attached all routers to fastapi app instance

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
 # registering routes and main entry point of project 
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from app.documents.router import router as document_router
-from app.auth.router import router as auth_router
-from app.db.base import Base
-from app.db.session import engine
+from app.routers.document import router as document_router
+from app.routers.auth import router as auth_router
+from app.routers.ai import router as ai_router
+from app.utils.ai import initialize_rag_pipeline
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
+    app.state.rag = initialize_rag_pipeline()
     yield
 
 app = FastAPI(title="UDP AI SaaS", lifespan=lifespan)
 
 app.include_router(auth_router)
 app.include_router(document_router)
+app.include_router(ai_router)
